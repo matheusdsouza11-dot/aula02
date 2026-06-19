326549803026c576a96fc3126cdbb11ff580befa
Matheus Dos Santos Souza - SENAI - Cuiaba/MT <matheus.dsantos@alunosenai.mt>
2026-06-19 10:01:59 -0400

    02

---
+++ 02.py
@@ -0,0 +1,5 @@
+peso = float(input("Peso (kg): "))
+altura = float(input("Altura (m): "))
+imc = peso / (altura ** 2)
+print(f"Seu IMC é: {imc:.2f}")
+
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("ola mundo")
+
+nome = input("Digite o seu nome: ")
+
+print(f"Olá, {nome}! Seja muito bem-vindo ao mundo do Python!")
